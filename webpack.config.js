e0e42cc638e8b605a9486bb106d618ef07c3aca4
Rynhardt Engelbrecht <107496937+rynhardt-engelbrecht@users.noreply.github.com>
2024-04-06 20:38:42 +0200

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Restaurant Page',
+      template: './src/index.html',
+    }),
+  ],
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
